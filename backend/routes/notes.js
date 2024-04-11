e030d7655cc082561fb48bdb17c503f06a730416
Satyajit <satyajitrakhunde@gmail.com>
2024-04-11 22:07:41 +0530

    able to update the notes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -8,13 +8,13 @@ router.use(express.json());
 
 // ROUTE 1: Get logged-in user details using GET "/api/notes/fetchallnotes". Login required
 router.get('/fetchallnotes', fetchuser, async (req, res) => {
-   try {
-       const notes = await Note.find({ user: req.user.id });  
-       res.json(notes);
-   } catch (error) {
-       console.error(error.message); 
-       res.status(500).json({ error: 'Internal server error' });
-   }
+    try {
+        const notes = await Note.find({ user: req.user.id });  
+        res.json(notes);
+    } catch (error) {
+        console.error(error.message); 
+        res.status(500).json({ error: 'Internal server error' });
+    }
 });
 
 // ROUTE 2: Add a new note using POST "/api/notes/addnote". Login required
@@ -41,6 +41,34 @@ router.post('/addnote', fetchuser, [
         
         const saveNote = await note.save();
         res.json(saveNote);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+// ROUTE 3: Update an existing note using PUT "/api/notes/updatenote/:id". Login required
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+    const { title, description, tag } = req.body;
+    // Create a newNote object
+    const newNote = {};
+    if (title) { newNote.title = title; }
+    if (description) { newNote.description = description; }
+    if (tag) { newNote.tag = tag; }
+
+    // Find the note to be updated and update it
+    try {
+        let note = await Note.findById(req.params.id);
+        if (!note) {
+            return res.status(404).send("Not found ");
+        }
+
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("Not allowed");
+        }
+
+        note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
+        res.json({ note });
     } catch (error) {
         console.error(error.message);
         res.status(500).json({ error: 'Internal server error' });
